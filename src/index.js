343ab8bd30b2e6cc7324a9baeaf2eb58b32aa0eb
yogurtsake <parkimothy@gmail.com>
2022-01-02 15:13:00 -0800

    added list view and animation on insert

--- src/index.js
+++ src/index.js
@@ -1,43 +1,19 @@
-// Make the DIV element draggable:
-dragElement(document.getElementById("mydiv"));
+const list = document.getElementById('list');
+const listItems = document.querySelectorAll('.list-item');
 
-function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  if (document.getElementById(elmnt.id + "header")) {
-    // if present, the header is where you move the DIV from:
-    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-  } else {
-    // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
-  }
+listItems.forEach((element, index) => {
+  element.addEventListener('click', function(e) {
+    console.log(index)
+    // console.log(Array.from(list.children).indexOf(element));
 
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
+    const detailsListItem = document.createElement('li');
+    detailsListItem.setAttribute('id', 'details');
 
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
+    // list.appendChild(detailsListItem)
+    list.insertBefore(detailsListItem, element.nextElementSibling);
+    
+    setTimeout(function () {
+      detailsListItem.className = detailsListItem.className + " fade show";
+    }, 10);
+  })
+})
