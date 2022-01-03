77561f70afb753f7844ad907d4773b4cf02a4d09
yogurtsake <parkimothy@gmail.com>
2022-01-02 16:35:33 -0800

    formated details cell with date and description input

--- src/index.js
+++ src/index.js
@@ -3,17 +3,48 @@ const listItems = document.querySelectorAll('.list-item');
 
 listItems.forEach((element, index) => {
   element.addEventListener('click', function(e) {
-    console.log(index)
-    // console.log(Array.from(list.children).indexOf(element));
+    if (!e.target.classList.contains('custom-checkbox')) {
+      // clear details view if it exists
+      if (element.nextElementSibling.id == 'details') {
+        element.nextElementSibling.classList.remove('show');
+        setTimeout(function () {
+          element.nextElementSibling.outerHTML = ''
+        }, 400);
+      } else {
+        var details = document.getElementById('details');
+        if (details !== null) {
+          details.classList.remove('show');
+          setTimeout(function () {
+            details.outerHTML = ''
+          }, 400);
+        };
 
-    const detailsListItem = document.createElement('li');
-    detailsListItem.setAttribute('id', 'details');
+        // console.log(Array.from(list.children).indexOf(element));
+        const detailsListItem = createDetailItem(new Date(), 'This is an example');
+        list.insertBefore(detailsListItem, element.nextElementSibling);
 
-    // list.appendChild(detailsListItem)
-    list.insertBefore(detailsListItem, element.nextElementSibling);
-    
-    setTimeout(function () {
-      detailsListItem.className = detailsListItem.className + " fade show";
-    }, 10);
+        setTimeout(function () {
+          detailsListItem.classList.add('show');
+        }, 10);
+      }
+    }
   })
 })
+
+function createDetailItem(date, notes) {
+  const detailsListItem = document.createElement('li');
+  detailsListItem.setAttribute('id', 'details')
+
+  const dateInput = document.createElement('input');
+  dateInput.setAttribute('type', 'date');
+  dateInput.valueAsDate = date;
+
+  const description = document.createElement('input');
+  description.setAttribute('type', 'text');
+  description.textContent = notes;
+
+  detailsListItem.appendChild(dateInput);
+  detailsListItem.appendChild(description);
+
+  return detailsListItem;
+}
