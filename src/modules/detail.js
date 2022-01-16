59209d3b50a66a1449cb3ca4b7fadb6c395d52f9
yogurtsake <parkimothy@gmail.com>
2022-01-15 18:25:29 -0800

    check for last list item. animations for remove and add detail view

--- src/modules/detail.js
+++ src/modules/detail.js
@@ -2,7 +2,6 @@ import { pubsub } from "./pubsub";
 
 export const detail = {
   render: (data) => {
-    console.log('rendering');
     const list = document.getElementById('list');
     const element = document.querySelector(`[data-id="${data.identifier}"]`);
 
@@ -11,17 +10,40 @@ export const detail = {
 
     const dateInput = document.createElement('input');
     dateInput.setAttribute('type', 'date');
-    dateInput.value = Date.parse(data['date']);
+    dateInput.value = data['date'];
 
     const description = document.createElement('input');
     description.setAttribute('type', 'text');
-    description.textContent = data['description'];
+    description.value = data['description'];
+    description.addEventListener('input', function(e) {
+      console.log('hello', data.identifier);
+    });
 
     detailsListItem.appendChild(dateInput);
     detailsListItem.appendChild(description);
 
-    var nextId = element.nextElementSibling.id;
-    // console.log(nextId); // left off here
+    var details = document.getElementById('details');
+    if (details !== null) {
+      details.classList.remove('show');
+      setTimeout(function () {
+        details.outerHTML = '';
+        console.log('detail !== null');
+      }, 100);
+    }
+
+    // if the li originally clicked did not have a detail view, add a detail view
+    if (element.nextElementSibling == null) {
+      list.appendChild(detailsListItem);
+      setTimeout(function () {
+        detailsListItem.classList.add('show');
+      }, 100);
+    } else if (element.nextElementSibling.id !== 'details') {
+      console.log('there wasnt details');
+      list.insertBefore(detailsListItem, element.nextElementSibling);
+      setTimeout(function () {
+        detailsListItem.classList.add('show');
+      }, 100);
+    }
   },
   getChanges: (title, description, date) => {
     console.log('hello');
